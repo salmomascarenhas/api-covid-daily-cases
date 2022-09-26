7de499f91040e3e324d9f7ed318955c8147b0a97
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-09-26 15:54:06 -0300

    fix parseCSV script

--- src/shared/parseCsv.ts
+++ src/shared/parseCsv.ts
@@ -1,26 +1,40 @@
 import * as csv from 'fast-csv'
-import { createReadStream } from "fs"
+import { createReadStream, existsSync } from "fs"
+import { CovidVariant } from '../database/entities/CovidVariant.entity'
 
-const parseCsv = async (path: string): Promise<any[]> => {
+const parseCsv = async (path: string): Promise<CovidVariant[]> => {
     return new Promise(function (resolve, reject) {
-        const array: any[] = []
+        const array: CovidVariant[] = []
+
+        if (!existsSync(path)) {
+            reject(null)
+        }
+
         createReadStream(path)
             .pipe(csv.parse({ headers: true }))
             .on('data', (row) => {
-                const obj: any = new Object()
-                Object.assign(obj, row)
+                const variantObj: CovidVariant = new CovidVariant()
 
-                array.push(obj)
+                variantObj.variant = row.variant
+                variantObj.location = row.location
+                variantObj.date = (new Date(row.date))
+                variantObj.num_sequences = parseInt(row.num_sequences)
+                variantObj.perc_sequences = parseFloat(row.perc_sequences)
+                variantObj.num_sequences_total = parseInt(row.num_sequences_total)
+
+                array.push(variantObj)
             })
             .on('end', () => {
+                console.log('CSV reading finished.')
+                if (!array) reject(new Error(`Could not process CSV.`))
+
                 resolve(array)
             })
             .on('error', (error) =>
-                reject(new Error(`Erro ao processar o CSV: ${error}`))
+                reject(new Error(`Error processing CSV:: ${error}`))
             )
     })
 }
-
 
 export { parseCsv }
 
