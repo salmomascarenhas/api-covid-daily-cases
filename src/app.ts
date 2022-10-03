a70ac1dcdc04500c5429c61fe6ab4e198e5c3a14
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-10-03 20:56:17 -0300

    Setting apicache with 5 minuntes.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import apicache from 'apicache'
 import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
@@ -9,6 +10,7 @@ import { routes } from './routes'
 import { parseCsv } from './shared/parseCsv'
 
 const app = express()
+let cache = apicache.middleware
 
 createConnection(process.env.POSTGRES_HOST).then(async (dataSource) => {
 
@@ -17,15 +19,20 @@ createConnection(process.env.POSTGRES_HOST).then(async (dataSource) => {
     const count = await ormRepository.count()
 
     if (dataSource.isInitialized && count < 1) {
+        // Parseing CSV with script.
         const variants: CovidVariant[] = await parseCsv(pathFile)
 
         dataSource.runMigrations()
+
+        // Saving dataframe at in database.
         await ormRepository.save(variants, { chunk: 1000 })
     }
 })
+    .then(() => { console.log('Database initialized!') })
     .catch(error => console.log(error))
-// .finally(() => { console.log('Database initialized!') })
+
 app.use(cors())
+app.use(cache('5 minutes'))
 app.use(express.json())
 app.use(routes)
 
