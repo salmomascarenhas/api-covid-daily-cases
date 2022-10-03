7fc55f345f56f10fab715f42885d84546e2e5dc1
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-10-03 19:13:33 -0300

    settings swagger ui and default catch error.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
-import { Request, Response, Router } from "express"
+
+import { NextFunction, Request, Response, Router } from "express"
 import { StatusCodes } from "http-status-codes"
+import swaggerUi from 'swagger-ui-express'
+import apiSchema from '../api.schema.json'
 import { covidVariantsRouters } from "./covidVariants.routes"
 
 const routes = Router()
@@ -7,9 +10,20 @@ const routes = Router()
 routes.get('/', (request: Request, response: Response) => {
     return response
         .status(StatusCodes.OK)
-        .send({ message: 'Backend Challenge 2021 🏅 - Covid Daily Cases' })
+        .json({ message: 'Backend Challenge 2021 🏅 - Covid Daily Cases' })
+})
+
+
+routes.use((error, req: Request, res: Response, next: NextFunction) => {
+    return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ status: 'INTERNAL_SERVER_ERROR', message: error.message })
 })
 
 routes.use(covidVariantsRouters)
+
+routes.use('/docs', swaggerUi.serve)
+
+routes.get('/docs', swaggerUi.setup(apiSchema))
 
 export { routes }
