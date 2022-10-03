640dc7cf22a39d3590928eb1d4af5f255ae790d2
Salmo Mascarenhas <salmo.cruz@gmail.com>
2022-10-03 00:10:47 -0300

    hotfix: fix type of date to format YYYY-MM-DD

--- src/database/entities/CovidVariant.entity.ts
+++ src/database/entities/CovidVariant.entity.ts
@@ -9,7 +9,7 @@ export class CovidVariant {
     @Column({ name: 'location' })
     location: string
 
-    @Column({ name: 'date' })
+    @Column({ name: 'date', type: 'date' })
     date: Date
 
     @Column({ name: 'variant' })
@@ -23,16 +23,6 @@ export class CovidVariant {
 
     @Column({ name: 'num_sequences_total' })
     num_sequences_total: number
-
-    // constructor({ location, date, variant, num_sequences, perc_sequences, num_sequences_total }) {
-    //     if (!this.id) this.id = uuid()
-    //     if (variant) this.variant = variant
-    //     if (location) this.location = location
-    //     if (date) this.date = (new Date(date))
-    //     if (num_sequences) this.num_sequences = parseInt(num_sequences)
-    //     if (perc_sequences) this.perc_sequences = parseFloat(perc_sequences)
-    //     if (num_sequences_total) this.num_sequences_total = parseInt(num_sequences_total)
-    // }
 
     constructor() {
         if (!this.id) this.id = uuid()
